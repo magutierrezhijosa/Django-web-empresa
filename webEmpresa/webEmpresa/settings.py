b66c2afed6f2f0ca6821bcb92881dd1224805baa
Soy Miguel <magutierrezhijosa@gmail.com>
2025-06-05 17:58:30 +0200

    Creacion de la app blog para nuestro proyecto con Django

--- webEmpresa/webEmpresa/settings.py
+++ webEmpresa/webEmpresa/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
     'core',
     'services.apps.ServicesConfig',
+
 ]
 
 MIDDLEWARE = [
